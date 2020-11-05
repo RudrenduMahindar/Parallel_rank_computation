d5c76b0e187c7eb4c8df276465a25c279a2d4d40
RudrenduMahindar <mahindar@usc.edu>
2020-11-05 01:06:42 -0800

    'adding-python-files-here'

---
+++ rank_compute_python.py
@@ -0,0 +1,15 @@
+
+
+def read_from_file():
+	print("elements have been read from file")
+
+
+
+def rank_compute():
+	print("starting rank computation in multithreading")
+
+
+
+if __name__ == "__main__": 
+	read_from_file()
+	rank_compute()
