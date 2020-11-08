8d24c35dd0aeff4c05a24b66093ea2d5f230f7ec
RudrenduMahindar <mahindar@usc.edu>
2020-11-07 17:18:00 -0800

    'added_number_to_list'

--- rank_compute_python.py
+++ rank_compute_python.py
@@ -1,8 +1,29 @@
 
 
 def read_from_file():
-	print("elements have been read from file")
+	print("reading numbers from file")
+	f = open('number.txt', 'r') 
+  
+	# Reading from the file 
+	content = f.readlines() 
+  
+	# Varaible for storing the sum 
+	a = 0
+	numbers_list = []
+	# Iterating through the content of the file
+	# each line has multiple numbers, take each number from each line 
+	for line in content: 
+		numbers = line.split()
+		for number in numbers: 
+			a = int(number)
+			numbers_list.append(a)
+			print("The number is:", a) 
 
+	print("numbers have been read from file")
+
+	print("numbers from list")
+	for i in numbers_list:
+		print(i)
 
 
 def rank_compute():
