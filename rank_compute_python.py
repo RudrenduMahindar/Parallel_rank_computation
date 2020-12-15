701b75791f0aec397c8e5e81660da6d79db024c4
RudrenduMahindar <mahindar@usc.edu>
2020-12-16 00:44:00 +0530

    'sorted_list'

--- rank_compute_python.py
+++ rank_compute_python.py
@@ -4,6 +4,7 @@ import threading
 numbers_list = []
 threads_list = []
 result_list = []
+sorted_num_list = []
 threadLock = threading.Lock()
 
 def read_from_file():
@@ -32,9 +33,16 @@ def read_from_file():
 		print(i)
 	print("numbers have been printed from list......")		
 
-def init_result_list():
+
+def init_rank_result_list():
 	for i in range(len(numbers_list)):
 		result_list.append(0)
+
+
+def init_sorted_num_list():
+	for i in range(len(numbers_list)):
+		sorted_num_list.append(0)
+
 
 def start_multithreading():
 	for i in range(len(numbers_list)):
@@ -43,7 +51,6 @@ def start_multithreading():
 		thread.start()
 	for thread in threads_list:  # iterates over the list of threads
 		thread.join()       # waits until the thread has finished work    	
-
 
 
 def rank_compute(thread_id):
@@ -55,21 +62,35 @@ def rank_compute(thread_id):
 			result_list[i] += 1   # shared list access synchronized
 			threadLock.release()			
 
+
 def show_result_ranks():
-	print("Results below......")
+	print("Rank results below......")
 	for i in range(len(numbers_list)):
 		logging.info("Number %s : Rank %s", numbers_list[i], result_list[i])		
+
+
+
+def show_sorted_list():
+	print("Sorted list based on rank...")
+	for i in range(len(numbers_list)):
+		sorted_num_list[result_list[i]] = numbers_list[i]
+
+	for i in range(len(sorted_num_list)):	
+		logging.info("%s : Number = %s", i, sorted_num_list[i]) 
+
 
 
 if __name__ == "__main__": 
 	format = "%(asctime)s: %(message)s"
 	logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
 	read_from_file()
-	init_result_list()
+	init_rank_result_list()
+	init_sorted_num_list()
 	print("starting rank computation in multithreading......")
 	start_multithreading()
 	print("ending rank computation now......")
 	print("showing results of rank computation......")
 	show_result_ranks()
-	print("finished rank_compute, results displayed......")
+	print("finished rank_compute, rank results displayed......")
+	show_sorted_list()
 	print("program ends......")
